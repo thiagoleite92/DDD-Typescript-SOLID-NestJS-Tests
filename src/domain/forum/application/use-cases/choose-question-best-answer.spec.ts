5b7d57485675fd7ab039b65acfd81525b428195e
Thiago Leite <leite.tjs@gmail.com>
2023-09-17 00:31:31 -0300

    feat: chose question best answer

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,53 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Use Case -> Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryQuestionsRepository,
+    )
+  })
+
+  it('should be able to choose question best answer by id', async () => {
+    const question = makeQuestion()
+
+    const answer = makeAnswer({ questionId: question.id })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: question.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
+  })
+
+  it('should be able to choose question best answer by id by wrong author question Id', async () => {
+    const question = makeQuestion({ authorId: new UniqueEntityID('author-1') })
+
+    const answer = makeAnswer({ questionId: question.id })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    expect(async () => {
+      await sut.execute({
+        answerId: answer.id.toString(),
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
