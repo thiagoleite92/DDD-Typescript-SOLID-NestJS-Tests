d976311f1cd9711d2678d3da1fb6b364cd1c79f5
Thiago Leite <leite.tjs@gmail.com>
2023-09-19 03:18:14 -0300

    feat: domain events and tests

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -4,12 +4,24 @@ export abstract class Entity<Props> {
   private _id: UniqueEntityID
   protected props: Props
 
+  protected constructor(props: Props, id?: UniqueEntityID) {
+    this.props = props
+    this._id = id ?? new UniqueEntityID()
+  }
+
   public get id() {
     return this._id
   }
 
-  protected constructor(props: Props, id?: UniqueEntityID) {
-    this.props = props
-    this._id = id ?? new UniqueEntityID()
+  public equals(entity: Entity<any>): boolean {
+    if (entity === this) {
+      return true
+    }
+
+    if (entity.id === this._id) {
+      return true
+    }
+
+    return false
   }
 }
--- src/core/entities/unique-entity-id.ts
+++ src/core/entities/unique-entity-id.ts
@@ -14,4 +14,8 @@ export class UniqueEntityID {
   toValue() {
     return this.value
   }
+
+  equals(id: UniqueEntityID): boolean {
+    return id.toValue() === this.value
+  }
 }
