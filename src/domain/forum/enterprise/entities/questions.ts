0e59d2bd813598f6d4676c7829f8a50330a843f4
Thiago Leite <leite.tjs@gmail.com>
2023-09-20 07:35:32 -0300

    feat: event question best answer

--- src/domain/forum/enterprise/entities/questions.ts
+++ src/domain/forum/enterprise/entities/questions.ts
@@ -4,6 +4,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
 import { QuestionAttachmentList } from './question-attachment-list'
+import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-chosen-event'
 
 export interface QuestionProps {
   authorId: UniqueEntityID
@@ -51,8 +52,12 @@ export class Question extends AggregateRoot<QuestionProps> {
     return this.props.bestAnswerId
   }
 
-  set bestAnswerId(bestAnwserId: UniqueEntityID | undefined) {
-    this.props.bestAnswerId = bestAnwserId
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
+      this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
+    }
+
+    this.props.bestAnswerId = bestAnswerId
     this.touch()
   }
 
